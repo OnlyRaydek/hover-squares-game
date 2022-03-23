b62f2f74eccd5154ae2f130dfa345384752deb1c
said_bayram <sbayram@conceptsoft.io>
2022-03-24 00:06:46 +0200

    added catch in apiGet

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,7 +1,25 @@
+const json = [
+  {
+    name: 'easy',
+    field: 5,
+  },
+  {
+    name: 'normal',
+    field: 15,
+  },
+  {
+    name: 'hard',
+    field: 25,
+  },
+];
+
 export const apiGet = (setState) => {
   fetch('http://demo7919674.mockable.io/')
   .then((response) => response.json())
   .then((json) => {
     setState(json);
+  }).catch(() => {
+    console.log('warn')
+    return json;
   })
 };
